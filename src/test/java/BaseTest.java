86be3a2a9dc8852144b44f893a81a9eb0970eeed
cmtn-86 <58218011+cmtn-86@users.noreply.github.com>
2023-02-02 23:42:45 +0000

    test1

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,15 +1,20 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
+    WebDriver driver = new ChromeDriver();
 
 @BeforeSuite
     public void setup(){
-    WebDriverManager.chromedriver().setup();
-    WebDriver driver = new ChromeDriver();
     System.out.println("Test startet");
+}
+@AfterSuite
+public void tearDown(){
+    System.out.println("TearDown");
+    driver.quit();
 }
 }
--- src/test/java/TC_001_xyz.java
+++ src/test/java/TC_001_xyz.java
@@ -1,8 +1,14 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class TC_001_xyz extends BaseTest{
     @Test
     public void deneme(){
         System.out.println("deneme");
+        driver.get("https://www.chess.com/");
+        WebElement x= driver.findElement(By.xpath("//*[@id=\"tb\"]/div[2]/a[1]"));
+        x.click();
     }
 }
