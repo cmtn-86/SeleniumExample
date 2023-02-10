57d554d069b9c78f82b612b34bdea02a3d6a84b1
cmtn-86 <58218011+cmtn-86@users.noreply.github.com>
2023-02-10 03:05:27 +0100

    asdas

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,20 +1,29 @@
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
-    WebDriver driver = new ChromeDriver();
+    WebDriver driver ;
 
-@BeforeSuite
+
+
+
+    @BeforeSuite
     public void setup(){
-    System.out.println("Test startet");
-}
-@AfterSuite
-public void tearDown(){
-    System.out.println("TearDown");
-    driver.quit();
-}
+        WebDriverManager.firefoxdriver().setup();
+        System.out.println("Test startet");
+        driver=new FirefoxDriver();
+    }
+    @AfterSuite
+    public void tearDown(){
+        System.out.println("TearDown");
+        driver.quit();
+        /////////
+        ///
+
+        ///asdasd
+    }
 }
--- src/test/java/TC_001_xyz.java
+++ src/test/java/TC_001_xyz.java
@@ -3,12 +3,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class TC_001_xyz extends BaseTest{
     @Test
     public void deneme(){
         System.out.println("deneme");
         driver.get("https://www.chess.com/");
-        WebElement x= driver.findElement(By.xpath("//*[@id=\"tb\"]/div[2]/a[1]"));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        WebElement x= driver.findElement(By.xpath("/html/body/div[1]/div[2]/main/div/div/section[1]/div[2]/div[2]/form/button/div[2]/div[1]"));
         x.click();
+        System.out.println("deneme");
     }
 }
